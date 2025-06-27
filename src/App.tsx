6dac6b04dd35b3c6db2e342828136222c4de89fb
abfree <exportx@abfree.com>
2025-06-27 18:38:03 +0800

    移动v2笨笨

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import { AllProviders } from "data";
-import OffileV1 from "./examples/OffileV1";
-import { BasicButtonDemo } from "./examples/BasicButtonDemo";
 
 function App() {
   return (
     <AllProviders>
-      <div style={{ padding: "20px" }}>
-        <BasicButtonDemo />
-        <hr style={{ margin: "40px 0" }} />
-        <OffileV1 />
-      </div>
+
     </AllProviders>
   );
 }
